c6a4507acf5e58c4cccf581695a7882d005b29bc
manasadevi03 <123718209+manasadevi03@users.noreply.github.com>
2023-12-28 13:00:05 +0530

    Update predict.py

--- Tabs/predict.py
+++ Tabs/predict.py
@@ -17,7 +17,7 @@ def app(df, X, y):
     st.markdown(
         """
             <p style="font-size:25px">
-                This app uses <b style="color:green">Random Forest Classifier</b> for the Prediction of Pneumonia Type and Intensity.
+                This app uses <b style="color:green">Decision Tree Classifier</b> for the Prediction of Pneumonia Type and Intensity.
             </p>
         """, unsafe_allow_html=True)
     
